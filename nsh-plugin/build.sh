ea158d46051c9f2d10dfd15895dc96cd2240de5a
Ed Warnicke <eaw@cisco.com>
2016-06-17 21:13:04 -0500

    Have verify.sh use build.sh so verify checks user code path
    Change-Id: Iadc3b1cec27d994daf6bc096c2230be19b565655
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- nsh-plugin/build.sh
+++ nsh-plugin/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+NSH_PLUGIN_DIR=$(dirname $0)
+NSH_INSTALL_PREFIX=${NSH_INSTALL_PREFIX:-/usr}
+cd ${NSH_PLUGIN_DIR}
 autoreconf -i -f
 [ -d build ] ||  mkdir build
+[ -d ${NSH_INSTALL_PREFIX} ] || mkdir -p ${NSH_INSTALL_PREFIX}
 cd build
-../configure --prefix /usr
+../configure --prefix ${NSH_INSTALL_PREFIX}
 make
 sudo make install
